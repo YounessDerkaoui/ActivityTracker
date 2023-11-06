f9b68c10d2a9107a076ff6ece6d988c9b32f0f24
youness.derkaoui <youness.derk@gmail.com>
2023-11-06 15:02:37 +0100

    Overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { OverviewComponent } from './overview/overview.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/overview/overview.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss']
+})
+export class OverviewComponent {
+
+  cardData = [
+    { title: 'Achievements', subtitle: 'Your latest achievements', imageSrc: '/assets/img/achievement.png', content: ['Run 5Km', 'Run 10Km'] , icon : 'verified', buttons : ['DETAILS'], share : true},
+    { title: 'Points Earned', subtitle: 'Total crypto currency earned', imageSrc: '/assets/img/earnings.jpg', content: ['1 (XRP)', '0.0023 (ETH)'], icon : 'score', buttons : ['DETAILS'], share : false},
+    { title: 'Leaderboard', subtitle: 'Your rank in comparison to other users', imageSrc: '/assets/img/leaderboard.jpg', content: ['Ranked \'15 256\' (Casablanca)', 'Ranked \'2 930 019\' (Morocco)'], icon : 'leaderboard', buttons : ['DETAILS'], share : true },
+    { title: 'Redeem Points', subtitle: 'Make transactions', imageSrc: '/assets/img/redeem.jpg', content: ['Exchange currency', 'Shop products'], icon : 'payment', buttons : ['WALLET'], share : false },
+    { title: 'Progress Tracking', subtitle: 'Set your goals', imageSrc: '/assets/img/progress.jpg', content: ['Hit the gym 4 days a week', 'Be consistent'], icon : 'timeline', buttons : ['DETAILS', 'ADD NEW'], share : false },
+    { title: 'Quote of the day', subtitle: 'Inspiring words', imageSrc: '/assets/img/quotes.jpg', content: ['"Less is More"'], icon : 'format_quote', buttons : [], share : true },
+    { title: 'Support and Help Center', subtitle: 'Customer support', imageSrc: '/assets/img/support.jpg', content: ['Ask for assistance', 'Contact us'], icon : 'live_help', buttons : ['HELP'], share : false },
+  ];
+
+}
